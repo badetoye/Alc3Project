10ff10107b0dcdf22c1569c7a40a7941cc943449
Bolaji Adetoye <badetoye@gmail.com>
2018-06-30 00:24:43 +0100

    update

--- designs.js
+++ designs.js
@@ -1,29 +1,30 @@
 
-// Variable declaration!
-let heigth, width , color;
 
 // When size is submitted by the user, call makeGrid() function
 $('#sizePicker').submit(function(event){
 	event.preventDefault();
-	heigth = $('#inputHeight').val();
-	width = $('#inputWeight').val();
+	const heigth = $('#inputHeight').val();
+	const width = $('#inputWeight').val();
 	makeGrid(heigth,width);
 
 })
-//implement the makeGrid() function and create a grid of squares
+//Implement the makeGrid() function and create a grid of squares
 function makeGrid(n,m) {
 	$('tr').remove();//remove previously created table rows
 
 	for(let i=1; i<=n; i++) {
+		//create a row
 		$('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+
 		for(let j=1; j<=m; j++) {
+			//add a td or cell to row
 			$('#table' + i).append('<td></td>');
 		}
 	}
 
-	//add color to cell
+	//add color to cell when clicked
 	$('td').click(function addColor() {
-		color = $('#colorPicker').val();
+		let color = $('#colorPicker').val();
 
 		if($(this).attr('style')) {
 			$(this).removeAttr('style')
