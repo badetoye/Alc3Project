7a1774e2ec707eeb558749d34113bb1c208f96af
Bolaji Adetoye <badetoye@gmail.com>
2018-07-01 09:43:25 +0100

    update

--- designs.js
+++ designs.js
@@ -5,14 +5,14 @@ $("input[type='submit']").on('click',function(event){
 	event.preventDefault();
 	const heigth = $('#inputHeight').val();
 	const width = $('#inputWeight').val();
-	
+
 	$('#pixelCanvas').empty();//select the table and empty it's child elements before makeGrid() is called.
 	makeGrid(heigth,width);
 
 })
 //Implement the makeGrid() function and create a grid of squares
 function makeGrid(n,m) {
-	
+
 	for(let i=1; i<=n; i++) {
 		//create a row
 		$('#pixelCanvas').append('<tr id=table' + i + '></tr>');
